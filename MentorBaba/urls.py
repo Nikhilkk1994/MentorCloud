572bdbd72d23ba568106666fbafd3cbfba301eb7
Nikhil <nkaplas@amazon.com>
2021-01-23 12:42:26 +0530

    update Media

--- MentorBaba/urls.py
+++ MentorBaba/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
 from package_session import urls as package_session_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include(package_session_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
